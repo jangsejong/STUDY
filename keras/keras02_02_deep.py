fb2a19814b9e0c634cd9c0173d3cefb5b8d50d60
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-11-24 09:38:47 +0900

    결과값수정

--- keras/keras02_02_deep.py
+++ keras/keras02_02_deep.py
@@ -9,11 +9,11 @@ y = np.array([1,2,3])
 
 #2. 모델구성 [hidden layer]
 model = Sequential()
-model.add(Dense(5, input_dim=1))
-model.add(Dense(7))          #하이퍼 파라미트 튜닝
-model.add(Dense(10))
-model.add(Dense(7))
-model.add(Dense(4))
+model.add(Dense(55, input_dim=1))
+model.add(Dense(77))          #하이퍼 파라미트 튜닝
+model.add(Dense(100))
+model.add(Dense(70))
+model.add(Dense(44))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
@@ -27,6 +27,6 @@ result = model.predict([4])
 print('4의 예측값 : ', result)
 
 '''
-loss :  3.2657339033903554e-05
-4의 예측값 :  [[3.9966502]]..
+loss :  0.0002002768887905404
+4의 예측값 :  [[3.98252]]
 '''
