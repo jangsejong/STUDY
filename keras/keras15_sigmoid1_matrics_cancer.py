345fef56b3b3be8d45ccd6d57eae721cc2308d34
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-11-29 14:51:24 +0900

    수정

--- keras/keras15_sigmoid1_matrics_cancer.py
+++ keras/keras15_sigmoid1_matrics_cancer.py
@@ -29,18 +29,18 @@ y = datasets.target
 #2. 모델구성
 
 model = Sequential()
-model.add(Dense(10, activation='linear', input_dim=30))
+model.add(Dense(10, activation='linear', input_dim=30)) # linear 원래값
 model.add(Dense(10, activation='sigmoid'))
 model.add(Dense(10, activation='linear'))
 model.add(Dense(1, activation='sigmoid'))
 model.add(Dense(6, activation='linear'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid')) # sigmoid 0,1 
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) # 훈련에 상황만 판단
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) # 훈련에 상황만 판단, 데이터가 한쪽에 편중되지 않아야 신뢰도가 높다
 
 from tensorflow.keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss',patience=50, mode='min', verbose=1, restore_best_weights=True)
+es = EarlyStopping(monitor='val_loss',patience=40, mode='min', verbose=1, restore_best_weights=True)
 
 start = time.time()
 hist = model.fit(x_train, y_train, epochs=2000, batch_size=10,verbose=1, validation_split=0.2, callbacks=[es])
@@ -48,15 +48,23 @@ hist = model.fit(x_train, y_train, epochs=2000, batch_size=10,verbose=1, validat
 #4. 평가, 예측
 loss = model.evaluate (x_test, y_test)
 print('loss :', loss) #loss :
-y_predict = model.predict(x_test)
-print(y_predict)
+#y_predict = model.predict(x_test)
+#print(y_predict)
 
 
 '''
 epochs=2000, patience=40 일때
-Epoch 126/2000
-loss : [0.2297917604446411, 0.8859649300575256] # 2개이상 리스트
+Epoch 151/2000
+loss : [0.2657780945301056, 0.9122806787490845]
 
-Epoch 92/2000
-loss : [0.30516454577445984, 0.8947368264198303]
+Epoch 126/2000
+loss : [0.32039958238601685, 0.8947368264198303] # 2개이상 리스트
+
+epochs=2000, patience=45 일때
+Epoch 106/2000
+loss : [0.2919485569000244, 0.8947368264198303]
+
+Epoch 151/2000
+loss : [0.2657780945301056, 0.9122806787490845]
+
 '''
