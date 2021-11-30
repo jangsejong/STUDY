3a3474051ab447f7880d73269929b4498090271e
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-11-30 19:21:29 +0900

    11/30_pm19:00

--- keras/keras18_kaggle2_bike.py
+++ keras/keras18_kaggle2_bike.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn import datasets
-from tensorflow.keras.utils import to_categorical
-from tensorflow.python.keras.metrics import accuracy
-from tensorflow.python.keras.utils.metrics_utils import result_wrapper
-from pandas import Series, DataFrame
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.ensemble import RandomForestClassifier
+
+def RMSE(y_test,y_pred):
+    return np.sqrt(mean_squared_error(y_test, y_pred))
+
 
 #1. 데이터
 path = "./_data/bike/"
@@ -15,57 +18,172 @@ train = pd.read_csv(path + "train.csv")# index_col=0,  header=0) # 인덱스 조
 #print(train)
 #print(train.shape) #(10886, 12)
 
-test = pd.read_csv(path + "test.csv") #index_col=0,  header=0)
+test_file = pd.read_csv(path + "test.csv") #index_col=0,  header=0)
 #gender_submission = pd.read_csv(path + "gender_submission.csv", index_col=0,  header=0)
 
 #print(test.shape) #(6493, 9)
-submit = pd.read_csv(path +'sampleSubmission.csv')
+submit_file = pd.read_csv(path +'sampleSubmission.csv')
 #print(submit.shape) #(6493, 2)
+#print(submit.columns)   #['datatime, count']
 
-print(type(train)) #<class 'pandas.core.frame.DataFrame'>
+
+#print(type(train)) #<class 'pandas.core.frame.DataFrame'>
+'''
 #print(train.info())  #object 모든 자료의 최상위형
 #print(train.describe()) #std 표준편차  mean, 중위값과 평균값차이
-
+결측값확인
+#train.info()
+.isna()
+.isnull()
+.notna()
+.notnull()
+#train[train.duplicated()] # 중복값 없음 
+#test[test.duplicated()] # 중복값 없음
 
 '''
-
-x_train = 
-y_train = np.arange
-y = pd.get_dummies(y)
+#print(train.head(3))
 
 
+
+x = train.drop(['datetime','casual','registered','count'], axis=1)
+test_file = test_file.drop(['datetime'], axis=1)
+
+print(x.columns)
+#Index(['season', 'holiday', 'workingday', 'weather', 'temp', 'atemp',
+#       'humidity', 'windspeed'],
+#      dtype='object')
+#print(x.shape)  (10886, 8)
+#submit_file = submit_file.drop(['datetime'], axis=1)
+y = train['count']
+#print(y.shape)  #(10886,)
+'''
+trainWind0=train.loc[train['windspeed']==0]
+trainWindNot0=train.loc[train['windspeed']!=0]
+def predict_windspeed(data): #(출력확인용)
+    #풍속 예측에 사용되는 변수 
+    wCol=['season','weather','humidity','temp','atemp']
+    
+    #풍속을 0인 것과 0이 아닌 것으로 구분
+    dataWind0=data.loc[data['windspeed']==0]
+    dataWindNot0=data.loc[data['windspeed']!=0]
+    
+    #랜덤포레스트 분류기 생성
+    rfModel=RandomForestClassifier()
+    dataWindNot0['windspeed']=dataWindNot0['windspeed'].astype("str")
+    
+    # wCol > 풍속학습 > 모델완성 
+    rfModel.fit(dataWindNot0[wCol], dataWindNot0['windspeed']) #(학습대상, 학습자료)
+    
+    #학습한 모델로 풍속 0에 대한 데이터 예측 
+    preValue=rfModel.predict(X=dataWind0[wCol])
+    print(preValue)
+    
+    #풍속이 0인 것과 0이 아닌 것으로 분류 
+    predictWind0=dataWind0
+    predictWindNot0=dataWindNot0
+    
+    #예측값을 풍속이 0인 데이터에 대입
+    predictWind0['windspeed']=preValue
+    #풍속이 0이 아닌 데이터와 풍속이 0인 데이터 병합하여 data에 대입
+    data=predictWindNot0.append(predictWind0)
+    return data
+
+def predict_windspeed2(data): #(실제데이터 보정용)
+    #풍속 예측에 사용되는 변수 
+    wCol=['season','weather','humidity','temp','atemp']
+    
+    #풍속을 0인 것과 0이 아닌 것으로 구분
+    dataWind0=data.loc[data['windspeed']==0]
+    dataWindNot0=data.loc[data['windspeed']!=0]
+    
+    #랜덤포레스트 분류기 생성
+    rfModel=RandomForestClassifier()
+    dataWindNot0['windspeed']=dataWindNot0['windspeed'].astype("str")
+    
+    # wCol > 풍속학습 > 모델완성 
+    rfModel.fit(dataWindNot0[wCol], dataWindNot0['windspeed']) #(학습대상, 학습자료)
+    
+    #학습한 모델로 풍속 0에 대한 데이터 예측 
+    preValue=rfModel.predict(X=dataWind0[wCol])
+    print(preValue)
+    
+    #풍속이 0인 것과 0이 아닌 것으로 분류 
+    predictWind0=dataWind0
+    predictWindNot0=dataWindNot0
+    
+    #예측값을 풍속이 0인 데이터에 대입
+    predictWind0['windspeed']=preValue
+    #풍속이 0이 아닌 데이터와 풍속이 0인 데이터 병합하여 data에 대입
+    data=predictWindNot0.append(predictWind0)
+
+    # 행이름 reset(data만 따로 가져오기 위함)
+    data.reset_index(inplace=True)
+    data.drop('index',inplace=True, axis=1)
+    return data
+fig, ax1 = plt. subplots()
+plt.sca(ax1)
+plt.xticks(rotation=30)
+
+#sns.countplot(data=train, x="windspeed", ax=ax1)
+'''
+
+#데이타 로그변환 : y 값이 한쪽으로 몰릴시 통상 사용
+y = np.log1p(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=66)
 
 
 
 #2. 모델구성
 
 model = Sequential()
-model.add(Dense(30, activation='linear', input_dim=12))
-model.add(Dense(30, activation='linear'))
-model.add(Dense(18, activation='linear'))
-model.add(Dense(6, activation='linear'))
+model.add(Dense(250, input_dim=8))
 model.add(Dense(4, activation='linear'))
-model.add(Dense(2, activation='linear'))
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(15, activation='linear'))
+model.add(Dense(1))
 
 
 
 
 #3. 컴파일, 훈련
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-from tensorflow.keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss',patience=200, mode='min', verbose=1)
-
-
-hist = model.fit(x_train, y_train, epochs=2000, batch_size=54, validation_split=0.2, callbacks=[es]) # batch_size=default 는 32이다.
+model.compile(loss='mse', optimizer='adam') #rms
+model.fit(x_train, y_train, epochs=10, batch_size=8, validation_split=0.2, verbose=1) # batch_size=default 는 32이다.
 
 #4. 평가, 예측
 loss = model.evaluate (x_test, y_test)
-print('loss :', loss[0]) #loss : 낮은게 좋다
-print('accuracy :', loss[1])
-results = model.predict(x_test[:4])
-print(y_test[:4])
-print(results)
+print('loss :', loss) #loss :
+y_pred = model.predict(x_test)
+
+rmse = RMSE(y_test, y_pred)
+print('RMSE :', rmse) #loss :
+
+
+r2 = r2_score(y_test, y_pred)
+print('r2score :', r2) 
 
 '''
+loss : 23708.40234375
+RMSE : 153.9753502965621
+r2score : 0.24991743293736246
+
+--------
+y 값 로그변환시
+loss : 1.4570910930633545
+RMSE : 1.207100372905771
+r2score : 0.256417033659474
+
+
+loss : 24206.748046875
+RMSE : 155.58518026825467
+r2score : 0.2424443511977793
+'''
+
+
+###################제출용 제작############################
+results = model.predict(test_file)
+
+submit_file['count'] = results
+
+print(submit_file[:10])
+
+submit_file.to_csv(path + 'submit_test10.csv', index = False)
