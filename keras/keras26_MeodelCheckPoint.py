8ea1b2b949c863d18d4bdbef24ea5adf80e51541
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-12-06 15:01:42 +0900

    12/06

---
+++ keras/keras26_MeodelCheckPoint.py
@@ -0,0 +1,98 @@
+from sklearn.datasets import load_boston
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import numpy as np
+from tensorflow.keras.models import Sequential, Model, load_model
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+import matplotlib.pyplot as plt
+import time as time
+from tensorflow.python.keras.callbacks import History
+
+
+#1. 데이터
+datasets = load_boston()
+x = datasets.data
+y = datasets.target
+print(np.min(x), np.max(x))  #0.0  711.0   
+
+# x = x/711.             #. 안전하다
+# x = x/np.max(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=True, random_state=66)  #shuffle 은 기본값 True
+#x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1875, shuffle=True, random_state=66)
+
+#scaler = MinMaxScaler()
+#scaler = StandardScaler()
+#scaler = RobustScaler()
+scaler = MaxAbsScaler()
+
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+#2. 모델구성
+
+model = Sequential()
+model.add(Dense(55, input_dim=13))
+model.add(Dense(50))
+model.add(Dense(45))
+model.add(Dense(40, activation='relu'))
+model.add(Dense(35))
+model.add(Dense(30))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(20))
+model.add(Dense(15))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(5))
+model.add(Dense(2))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint # EarlyStopping patience(기다리는 횟수)
+
+es = EarlyStopping(monitor='val_loss', patience=10, mode = 'min', verbose = 1)
+mcp = ModelCheckpoint(monitor='val_loss', mode= 'auto', verbose=1, save_best_only=True, filepath='./_ModelCheckPoint/keras26_1_MCP.hdf5')
+
+start = time.time()
+hist = model.fit(x_train, y_train, epochs=1000, batch_size = 13, 
+                 validation_split = 0.2 , callbacks = [es, mcp])
+end = time.time()- start
+
+
+print("---------------------------------")
+print(hist)
+print("---------------------------------")
+print(hist.history)
+print("---------------------------------")
+print(hist.history['loss'])
+print("---------------------------------")
+print(hist.history['val_loss'])
+print("---------------------------------")
+
+plt.figure(figsize = (9, 5))
+plt.plot(hist.history['loss'], marker =',',c='red',label='loss')
+plt.plot(hist.history['val_loss'], marker =',',c='blue',label='val_loss')
+plt.grid()
+plt.title('loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(loc = 'upper right')
+plt.show()
+
+#print("걸린시간 : ", round(end, 3), '초')
+
+model.save_weights("./_save/keras25_1_save_weights.h5")
+
+
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print('loss: ', loss)
+
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test, y_predict)
+print('r2스코어 : ', r2)
