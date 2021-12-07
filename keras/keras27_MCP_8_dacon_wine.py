b9d465e92d1aea40e0546a074b01dc2dd7cd0952
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-12-07 11:21:10 +0900

    12/07_am

--- keras/keras27_MCP_8_dacon_wine.py
+++ keras/keras27_MCP_8_dacon_wine.py
@@ -1,14 +1,20 @@
+from tensorflow.keras.models import Sequential, Model,  load_model
+from tensorflow.keras.layers import Dense, Input
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Input
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import EarlyStopping
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from tensorflow.python.keras.callbacks import ModelCheckpoint
+import time
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, LabelEncoder, OneHotEncoder
+from pandas import get_dummies
 
-#1. 데이터 분석
+import datetime
+date = datetime.datetime.now()
+datetime = date.strftime("%m%d_%H%M")
+#print(datetime)
+filepath = './_ModelCheckPoint/'
+filename = '{epoch:05d}={val_loss:.5f}.hdf5'
+model_path = "".join([filepath, 'k24', datetime,'_', filename])
+
+#1 데이터
 path = "../../_data/dacon/wine/"  
 train = pd.read_csv(path +"train.csv")
 test_file = pd.read_csv(path + "test.csv") 
@@ -17,54 +23,108 @@ submission = pd.read_csv(path+"sample_Submission.csv") #제출할 값
 y = train['quality']
 x = train.drop(['id', 'quality'], axis =1)
 
-
 le = LabelEncoder()                 # 라벨 인코딩은 n개의 범주형 데이터를 0부터 n-1까지 연속적 수치 데이터로 표현
 label = x['type']
 le.fit(label)
 x['type'] = le.transform(label)         # type column의 white, red > 0,1로 변환
 
+from tensorflow.keras.utils import to_categorical
 
-scaler = StandardScaler()
+test_file = test_file.drop(['id'], axis=1)
+label2 = test_file['type']
+le.fit(label2)
+test_file['type'] = le.transform(label2)
+
+y = train['quality']      # [6 7 5 8 4]             
+y = get_dummies(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.2, shuffle = True, random_state =66)
+
+#scaler = MinMaxScaler()
+#scaler = StandardScaler()
+scaler = RobustScaler()
+#scaler = MaxAbsScaler()
+
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test) 
+x_test = scaler.transform(x_test)
+test_file = scaler.transform(test_file)
 
-#2. 모델구성
-model = Sequential()
-model.add(Dense(30, activation='linear', input_dim=8))
-model.add(Dense(30, activation='linear'))
-model.add(Dense(18, activation='linear'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(4, activation='linear'))
-model.add(Dense(2, activation='linear'))
-model.add(Dense(1, activation='softmax'))
-
-#3. 컴파일
-model.compile(loss='mse', optimizer='adam')
-
-import datetime
-date = datetime.datetime.now()
-datetime = date.strftime("%m%d_%H%M") 
-
-filepath = './_ModelCheckPoint/'
-filename = '{epoch:04d}-{val_loss:.4f}.hdf5'      
-model_path = "".join([filepath, '8_dacon_wine_', datetime, '_', filename])
+#2 모델구성
+input1 = Input(shape=(12,))
+dense1 = Dense(30)(input1)
+dense2 = Dense(30)(dense1)
+dense3 = Dense(16)(dense2)
+dense4 = Dense(8)(dense3)
+dense5 = Dense(5)(dense4)
+dense6 = Dense(5)(dense5)
+ouput1 = Dense(5, activation='softmax')(dense6)
+model = Model(inputs=input1, outputs=ouput1)
 
 
-es = EarlyStopping(monitor="val_loss", patience=20, mode='min', verbose=1, restore_best_weights=True)
-mcp = ModelCheckpoint(monitor='val_loss', mode='auto', save_best_only=True, filepath=model_path)
-model.fit(x_train, y_train, epochs=1000, batch_size=32, verbose=2, validation_split=0.2, callbacks=[es, mcp])
+#3. 컴파일, 훈련
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics=['accuracy']) 
 
-#4. 결과
-loss = model.evaluate(x_test, y_test)
-print('loss : ', loss)
-y_pred = model.predict(x_test)
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience= 1000 , mode = 'auto', verbose=1, restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode= 'auto', verbose=1, save_best_only=True, filepath='./_ModelCheckPoint/keras24_Dacon_MCP.hdf5')
 
-from sklearn.metrics import r2_score 
-r2 = r2_score(y_test, y_pred)
-print("r2스코어", r2)
 
-def RMSE(y_test, y_pred): 
-    return np.sqrt(mean_squared_error(y_test, y_pred))   
-rmse = RMSE(y_test, y_pred) 
-print("RMSE : ", rmse)
+model.fit(x_train, y_train, epochs = 1000, batch_size = 12, validation_split=0.2, callbacks=[es,mcp])
+
+
+#4 평가예측
+loss = model.evaluate(x_test,y_test)
+print("loss : ",loss[0])                      # List 형태로 제공된다
+print("accuracy : ",loss[1])
+
+################################ 제출용 ########################################
+result = model.predict(test_file)
+result_recover = np.argmax(result, axis=1).reshape(-1,1) + 4
+submission['quality'] = result_recover
+
+submission.to_csv(path + "012.csv", index = False)
+
+
+'''
+MinMax
+loss :  1.01213538646698
+accuracy :  0.5548686385154724
+robust
+loss :  1.01096773147583
+accuracy :  0.5564142465591431
+MaxAbs
+loss :  1.007688045501709
+accuracy :  0.5502318143844604
+Standard
+loss :  0.9866795539855957
+accuracy :  0.5734157562255859
+
+
+002
+loss :  0.9862772822380066
+accuracy :  0.5765069723129272
+
+003
+loss :  1.0153590440750122
+accuracy :  0.5842349529266357
+
+004
+loss :  0.9992575645446777
+accuracy :  0.5888717174530029
+
+005
+loss :  1.0092556476593018
+accuracy :  0.5919629335403442
+
+006
+loss :  0.9349533915519714
+accuracy :  0.6018518805503845
+
+007
+loss :  0.9498463273048401
+accuracy :  0.6419752836227417
+
+'''
+
