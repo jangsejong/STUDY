b80779cfa828c4958a954b8e38ea67b1476eaa93
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-12-18 17:16:36 +0900

    Update keras999_stock.py

--- keras/keras999_stock.py
+++ keras/keras999_stock.py
@@ -153,14 +153,14 @@ from tensorflow.keras.layers import Dense, Input
 
 #2-1. 모델
 input1 = Input(shape=(5,))
-dense1 = Dense(100, activation='relu', name='dense1')(input1)
+dense1 = Dense(8, activation='relu', name='dense1')(input1)
 dense2 = Dense(4, activation='relu', name='dense2')(dense1)
 dense3 = Dense(2, activation='relu', name='dense3')(dense2)
 output1 = Dense(1, activation='relu', name='output1')(dense3)
 
 #2-2. 모델
 input2 = Input(shape=(5,))
-dense11 = Dense(100, activation='relu', name='dense11')(input2)
+dense11 = Dense(8, activation='relu', name='dense11')(input2)
 dense12 = Dense(6, activation='relu', name='dense12')(dense11)
 dense13 = Dense(4, activation='relu', name='dense13')(dense12)
 dense14 = Dense(2, activation='relu', name='dense14')(dense13)
@@ -187,7 +187,7 @@ model = Model(inputs=[input1, input2], outputs= [last_output1, last_output2])
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mae']) #rms
-model.fit([x1_train, x2_train], [y1_train,y2_train], epochs=1000, batch_size=1, validation_split=0.2, verbose=1) 
+model.fit([x1_train, x2_train], [y1_train,y2_train], epochs=100, batch_size=1, validation_split=0.2, verbose=1) 
 
 #4. 평가, 예측
 loss = model.evaluate ([x1_test, x2_test], [y1_test,y2_test], batch_size=1)
