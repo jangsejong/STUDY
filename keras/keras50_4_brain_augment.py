3fc2abf9665442e713cad49dfbd242b33b946a25
jangsejong <92250663+jangsejong@users.noreply.github.com>
2021-12-29 20:47:32 +0900

    12/29_night

--- keras/keras50_4_brain_augment.py
+++ keras/keras50_4_brain_augment.py
@@ -1,8 +1,3 @@
-#훈련데이터 10만개로 증폭
-#완료후 기존모델과 비교
-# save_dir도 _temp 에 넣고
-#중복데이터는 temp 에 저장후 훈련 끝난후 결과 보고 삭제
-
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
@@ -10,20 +5,29 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_im
 #1. Data
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True, 
-    # vertical_flip=True,
+    rescale = 1./255,
+    horizontal_flip=False,
+    vertical_flip=False,
     width_shift_range=0.1,
     height_shift_range=0.1,
-    # rotation_range=5, 
+    rotation_range=5,
     zoom_range=0.1,
-    # shear_range=0.7,
-    fill_mode='nearest'
+    shear_range=0.1,
+    fill_mode='nearest',
+    
+    featurewise_center=True,
+    featurewise_std_normalization=True,  
+    validation_split=0.1
 )
 
 test_datagen = ImageDataGenerator(                  #평가만 해야하기 때문에 증폭할 필요성이 없다.
     rescale=1./255
 )                                   #Found 160 images belong to 2 classes.
+
+
+
+
+
 
 # D:\_data\image\brain
 
@@ -32,102 +36,125 @@ xy_train = train_datagen.flow_from_directory(
     target_size=(150,150),  #사이즈조절가능
     batch_size=5,
     class_mode='binary',
-    shuffle=True
-)
+    shuffle=True,
+    subset='training'
+)                                     #Found 160 images belong to 2 classes.
+
 
 xy_test = train_datagen.flow_from_directory(
     '../_data/image/brain/test',
     target_size=(150,150),  #사이즈조절가능
     batch_size=5,
-    class_mode='binary'
-)                            
+    class_mode='binary',
+    subset='validation'
+)                                   #Found 120 images belong to 2 classes.
 
-augment_size = 50000
-randidx = np.random.randint(x_train.shape[0], size = augment_size)  #randint
-# print(x_train.shape[0])                   # 50000
+print(xy_train[0][0].shape)   #(10, 150, 150, 3)
+print(xy_test[0][0].shape)     #(10, 150, 150, 3)
+
+
+
+augment_size = 160
+randidx = np.random.randint(xy_train[0][0].shape[0], size = augment_size)  #randint
+# 0부터 x_train.shape[0] 까지의 값을 argument_size 만큼 뽑겠다 -> 리스트형태
+# print(x_train.shape[0])                   # 60000
 # print(randidx)                            # [28809 11925 51827 ... 34693  6672 48569]
-# print(np.min(randidx), np.max(randidx))   # 0 49998
+# print(np.min(randidx), np.max(randidx))   # 0 59998
 
-x_augmented = x_train[randidx].copy()
-y_augmented = y_train[randidx].copy()
-print(x_augmented.shape)                   # (50000, 32, 32, 3)
-print(y_augmented.shape)                   # (50000, 1)
+x_argumented = xy_train[0][0][randidx].copy()
+y_argumented = xy_train[0][1][randidx].copy()
 
+# print(x_argumented.shape)                # (40000, 28, 28)
+# print(y_augmented.shape)                   # (40000, )  ?
 
-x_augmented = x_augmented.reshape(x_augmented.shape[0],x_augmented.shape[1],x_augmented.shape[2],x_augmented.shape[3])
-x_train = x_train.reshape(50000,32,32,3)
-x_test = x_test.reshape(x_test.shape[0],32,32,3)
-print(x_augmented.shape) #(50000, 32, 32, 3) 
+xy_train = np.concatenate((xy_train, xy_augmented))
 
-
-
-x_train = np.concatenate((x_train, x_augmented))
-y_train = np.concatenate((y_train, y_augmented))
-
-print(x_train.shape)  #(100000, 32, 32, 3)            
-print(y_train.shape)  #(100000, 1)
+'''
+print(xy_train.shape)  #(100000, 28, 28, 1)              
 
 
-#모델
-from tensorflow.keras.models import *
-from tensorflow.keras.layers import *
+
+
+
+
+
+
+x_augmented = x_augmented.reshape(x_augmented.shape[0],x_augmented.shape[1],x_augmented.shape[2],1)
+x_train = x_train.reshape(60000,28,28,1)
+x_test = x_test.reshape(x_test.shape[0],28,28,1)
+
+
+
+
+
+#2. 모델
+from tensorflow.keras.models import Sequential, Model, load_model, Model,save_model
+from keras.layers import *
 
 model = Sequential()
-model.add(Conv2D(64, kernel_size=(3,3), padding='same', input_shape=(32,32,3), activation='relu'))
+model.add(Conv2D(8, kernel_size=(3,3), padding='same', input_shape=(150,150,3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 # model.add(Dropout(0.2))
 
 model.add(Conv2D(32, kernel_size=(3,3),padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-# model.add(Dropout(0.3))
+# model.add(Dropout(0.2))
 
-model.add(Conv2D(16, kernel_size=(3,3), padding='same', activation='relu'))
+model.add(Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-# model.add(Dropout(0.5))
+# model.add(Dropout(0.2))
 
 model.add(Flatten())
-model.add(Dense(64, activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(100, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
+
+mpdel = np.save("./_save_npy/keras47_5_save_model.h5")
+
+#model.summary()
 
 
-#컴파일,훈련
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['acc'])
+model.compile(loss='binary_crossentropy',
+             optimizer='adam',
+             metrics=['accuracy'])
 
-model.fit(x_train,y_train, epochs=10,steps_per_epoch = len(x_train))
-                    # validation_data = validation_generator,
-                    # validation_steps = 400,)
+import time
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience= 5 , mode = 'auto', verbose=1, restore_best_weights=True)
+
+start = time.time()
+             
+hist = model.fit_generator(xy_train, epochs=50, steps_per_epoch=32,
+                    validation_data=xy_test,
+                    validation_steps=4 )                  #과제
+                    # 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수를 지정합니다. 
+                    # 총 160 개의 검증 샘플이 있고 배치사이즈가 5이므로 4의 배수스텝으로 지정합니다.
+end = time.time()- start
 
 
 
-#평가
-
-# loss, acc = model.evaluate(x_test,y_test)
-# print("Accuracy : ", str(np.round(acc ,2)*100)+ "%")
-# #TypeError: 'float' object is not subscriptable
-
-# test_loss, test_acc = model.evaluate(x_test,y_test)
-# print('loss :', test_loss)
-# print('acc :', test_acc)
+# model.fit(xy_train[0][0],xy_train[0][1])
 
 loss = model.evaluate(x_test,y_test)
 print('loss : ', loss[0])
 print('accuracy:', loss[1])
 
 y_predict=model.predict(x_test)
-
+print(y_predict.shape, y_test.shape)
+print(y_predict[0])
+print(y_test[0])
 # print(y_test)
 # print(y_test.shape, y_predict.shape)
 
 y_predict=np.argmax(y_predict,axis=1)
-y_test=np.argmax(y_test, axis=1)
+# y_test=np.argmax(y_test,axis=1)
+# print(y_predict.shape, y_test.shape)
 
 from sklearn.metrics import r2_score, accuracy_score
 
 a=accuracy_score(y_test, y_predict)
 print('accuracy score:' , a)
-'''
+
+
 '''
 
-                   
-                 
