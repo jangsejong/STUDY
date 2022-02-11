37ace2bd1a278a172949a68e67055b0e1911715e
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-02-11 11:07:15 +0900

    0211

--- tf114/tf17_xor2.py
+++ tf114/tf17_xor2.py
@@ -7,14 +7,31 @@ def relu(x):
 x_data = [[0,0],[0,1],[1,0],[1,1]]
 y_data = [[0],[1],[1],[0]]
 
+
+# input_layer 
 x = tf.compat.v1.placeholder(tf.float32, shape = [None, 2])
 y = tf.compat.v1.placeholder(tf.float32, shape = [None, 1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,1]), name='weight')
-b = tf.compat.v1.Variable(tf.compat.v1.random.normal([1]), name = 'bias')
+w1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,8]), name='weight1')
+b1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8]), name = 'bias1')
+
+
 
 #2. 모델구성
-hypothesis = tf.nn.relu(tf.sigmoid(tf.matmul(x , w) + b))
+
+hedden_layer1 = tf.sigmoid(tf.matmul(x , w1) + b1)
+# hedden_layer1 = tf.matmul(x , w1) + b1
+# hedden_layer1 = tf.nn.relu(tf.sigmoid(tf.matmul(x , w1) + b1))
+
+w2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8,1]), name='weight2')
+b2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([1]), name = 'bias2')
+
+hypothesis = tf.sigmoid(tf.matmul(hedden_layer1, w2) + b2)
+
+
+# hypothesis = tf.nn.relu(tf.sigmoid(tf.matmul(x , w) + b))
+
+
 
 #3 - 1. 컴파일
 # loss = tf.reduce_mean(tf.square(hypothesis - y)) # mse
@@ -27,9 +44,11 @@ sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
 for i in range(2001):
-    loss_val,hy_val,b_val,_ = sess.run([loss, hypothesis, b, train], feed_dict = {x : x_data, y : y_data})
-    if i % 20 == 0:
-        print(i,'\t','loss : ', loss_val, '\n',hy_val, b_val)
+    loss_val,hy_val,_ = sess.run([loss, hypothesis, train], feed_dict = {x : x_data, y : y_data})
+    if i % 200 == 0:
+        print(i,'\t','loss : ', loss_val, '\n',hy_val)
+
+
 
 from sklearn.metrics import r2_score, mean_absolute_error
 
--- tf114/tf17_xor3.py
+++ tf114/tf17_xor3.py
@@ -7,18 +7,38 @@ def relu(x):
 x_data = [[0,0],[0,1],[1,0],[1,1]]
 y_data = [[0],[1],[1],[0]]
 
+
+# input_layer 
 x = tf.compat.v1.placeholder(tf.float32, shape = [None, 2])
 y = tf.compat.v1.placeholder(tf.float32, shape = [None, 1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,1]), name='weight')
-b = tf.compat.v1.Variable(tf.compat.v1.random.normal([1]), name = 'bias')
+w1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,8]), name='weight1')
+b1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8]), name = 'bias1')
+
+
 
 #2. 모델구성
-hypothesis = tf.nn.relu(tf.sigmoid(tf.matmul(x , w) + b))
+#h-1
+hedden_layer1 = tf.sigmoid(tf.matmul(x , w1) + b1)
+# hedden_layer1 = tf.matmul(x , w1) + b1
+# hedden_layer1 = tf.nn.relu(tf.sigmoid(tf.matmul(x , w1) + b1))
+
+w2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8,2]), name='weight2')
+b2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2]), name = 'bias2')
+
+
+
+#h-2
+hedden_layer2 = tf.sigmoid(tf.matmul(hedden_layer1, w2) + b2)
+w3 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,1]), name='weight3')
+b3 = tf.compat.v1.Variable(tf.compat.v1.random.normal([1]), name = 'bias3')
+
+
+output_layer = tf.sigmoid(tf.matmul(hedden_layer1, w3) + b3)
 
 #3 - 1. 컴파일
 # loss = tf.reduce_mean(tf.square(hypothesis - y)) # mse
-loss = -tf.reduce_mean(y*tf.log(hypothesis)+(1-y)*tf.log(1-hypothesis)) # binary_crossentropy
+loss = -tf.reduce_mean(y*tf.log(output_layer)+(1-y)*tf.log(1-output_layer)) # binary_crossentropy
 optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate = 0.1)
 train = optimizer.minimize(loss)
 
@@ -27,14 +47,16 @@ sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
 for i in range(2001):
-    loss_val,hy_val,b_val,_ = sess.run([loss, hypothesis, b, train], feed_dict = {x : x_data, y : y_data})
-    if i % 20 == 0:
-        print(i,'\t','loss : ', loss_val, '\n',hy_val, b_val)
+    loss_val,hy_val,_ = sess.run([loss, output_layer, train], feed_dict = {x : x_data, y : y_data})
+    if i % 200 == 0:
+        print(i,'\t','loss : ', loss_val, '\n',hy_val)
+
+
 
 from sklearn.metrics import r2_score, mean_absolute_error
 
 #4. 평가, 예측
-y_predict = tf.cast(hypothesis > 0.5, dtype = tf.float32) #tf.cast = 함수안의 조건식이 True 면 1.0 False면 0.0
+y_predict = tf.cast(output_layer > 0.5, dtype = tf.float32) #tf.cast = 함수안의 조건식이 True 면 1.0 False면 0.0
 # print(sess.run(hypothesis > 0.5, feed_dict = {x : x_data, y : y_data}))
 accuracy = tf.reduce_mean(tf.cast(tf.equal(y,y_predict),dtype = tf.float32))
 y_predict_data,acc = sess.run([y_predict,accuracy], feed_dict = {x : x_data, y : y_data})
