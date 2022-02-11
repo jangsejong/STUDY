94bef2d291283bfc33debd6bfef977c6ebac96b9
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-02-11 10:55:47 +0900

    0211

--- tf114/tf17_xor.py
+++ tf114/tf17_xor.py
@@ -12,8 +12,8 @@ y_data = [[0],[1],[1],[0]]
 x = tf.compat.v1.placeholder(tf.float32, shape = [None, 2])
 y = tf.compat.v1.placeholder(tf.float32, shape = [None, 1])
 
-w1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,3]), name='weight1')
-b1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([3]), name = 'bias1')
+w1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([2,8]), name='weight1')
+b1 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8]), name = 'bias1')
 
 
 
@@ -23,7 +23,7 @@ hedden_layer1 = tf.sigmoid(tf.matmul(x , w1) + b1)
 # hedden_layer1 = tf.matmul(x , w1) + b1
 # hedden_layer1 = tf.nn.relu(tf.sigmoid(tf.matmul(x , w1) + b1))
 
-w2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([3,1]), name='weight2')
+w2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([8,1]), name='weight2')
 b2 = tf.compat.v1.Variable(tf.compat.v1.random.normal([1]), name = 'bias2')
 
 hypothesis = tf.sigmoid(tf.matmul(hedden_layer1, w2) + b2)
