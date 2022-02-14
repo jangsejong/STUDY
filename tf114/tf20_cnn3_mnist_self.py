36835e42dc87a3d239f61ddb26f11455705df56b
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-02-14 14:33:09 +0900

    0214

--- tf114/tf20_cnn3_mnist_self.py
+++ tf114/tf20_cnn3_mnist_self.py
@@ -59,47 +59,39 @@ print(L4_maxpool)
 L_flat = tf.reshape(L4_maxpool, [-1, 2*2*16])
 print(L_flat) # Tensor("Reshape:0", shape=(?, 128), dtype=float32)
 
-#layer5 DNN
-w5 = tf.get_variable("w5", shape=[2*2*16, 64], initializer=tf.contrib.layers.xavier_initializer())
-b5 = tf.Variable(tf.random_normal([64]), name='b1')
-l5 = tf.nn.selu(tf.matmul(L_flat, w5) + b5)
-l5 = tf.nn.dropout(l5, keep_prob=0.7)
-print(l5) # Tensor("dropout/mul:0", shape=(?, 64), dtype=float32)
+#layer5
+L5 = tf.layers.dense(L_flat, 64, activation=tf.nn.relu)
+L5 = tf.layers.dropout(L5, rate=0.4)
 
-#layer6 DNN
-w6 = tf.get_variable("w6", shape=[64, 32], initializer=tf.contrib.layers.xavier_initializer())
-b6 = tf.Variable(tf.random_normal([32]), name='b1')
-l6 = tf.nn.relu(tf.matmul(l5, w6) + b6)
-l6 = tf.nn.dropout(l6, keep_prob=0.7)
-print(l6) # Tensor("dropout_1/mul:0", shape=(?, 32), dtype=float32)
+#layer6
+L6 = tf.layers.dense(L5, 32, activation=tf.nn.relu)
+L6 = tf.layers.dropout(L6, rate=0.4)
 
-#layer7 softmax
-w7 = tf.get_variable("w7", shape=[32, 10], initializer=tf.contrib.layers.xavier_initializer())
-b7 = tf.Variable(tf.random_normal([10]), name='b1')
-hypothesis = tf.nn.softmax(tf.matmul(l6, w7) + b7)
-print(hypothesis) # Tensor("add_1:0", shape=(?, 10), dtype=float32)
+#layer7
+L7 = tf.layers.dense(L6, 16, activation=tf.nn.relu)
 
-#3. 손실함수, 최적화, 예측
-loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
+#layer8
+L8 = tf.layers.dense(L7, 10, activation=tf.nn.softmax)
 
-sess = tf.compat.v1.Session()
-sess.run(tf.compat.v1.global_variables_initializer())
+#3. 컴파일, 훈련
+loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(L8), axis=1))
+optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 
-#4. 훈련
-for epochs in range(10):
-    avg_loss = 0
-    total_batch = int(x_train.shape[0] / 100) # 60000 / 100 = 600
-    for i in range(total_batch): # 600번 반복
-        batch_xs, batch_ys = x_train[i*100:(i+1)*100], y_train[i*100:(i+1)*100] # 100개씩 잘라서 배치
-        feed_dict = {x: batch_xs, y: batch_ys} 
-        batch_loss, _ = sess.run([loss, optimizer], feed_dict=feed_dict) # 훈련
-        avg_loss += batch_loss / total_batch # 평균 손실값
-    print('Epoch:', '%04d' % (epochs + 1), 'loss =', '{:.9f}'.format(avg_loss)) # 손실함수 값 출력
-
-#5. 평가
-accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(hypothesis, 1), tf.argmax(y, 1)), tf.float32)) # 정확도
-predict = tf.math.argmax(hypothesis, 1) # 예측값
-print('Accuracy:', sess.run(accuracy, feed_dict={x: x_test, y: y_test}))
-loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1)) # 손실함수
-print('Loss:', sess.run(loss, feed_dict={x: x_test, y: y_test}))
+#4. 세션
+with tf.Session() as sess: # with문을 사용하여 세션을 열어준다.
+      sess.run(tf.compat.v1.global_variables_initializer())
+      
+      for step in range(201):
+         _, loss_val = sess.run([optimizer, loss], feed_dict={x:x_train, y:y_train})
+         if step % 200 ==0:
+            print(step, loss_val) 
+            
+      results = sess.run(L8, feed_dict={x:x_test})
+      print(results, sess.run(tf.math.argmax(results, axis=1)))
+      
+      accuracy = sess.run(tf.reduce_mean(tf.cast(tf.math.equal(tf.math.argmax(results, axis=1), tf.math.argmax(y_test, axis=1)), dtype=tf.float32)))
+      pred = sess.run(tf.math.argmax(results, axis=1))
+      print("예측결과 : ", pred)
+      print("accuracy:", accuracy)
+      
+      sess.close()
