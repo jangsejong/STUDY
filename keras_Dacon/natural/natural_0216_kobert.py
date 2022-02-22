4d89e0b5bff9c780de8d201d6feb4ea7bdf3b58e
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-02-22 09:46:35 +0900

    0222

--- keras_Dacon/natural/natural_0216_kobert.py
+++ keras_Dacon/natural/natural_0216_kobert.py
@@ -13,218 +13,218 @@ test = pd.read_csv(os.path.join(PATH, 'test_data.csv'), encoding='utf-8')
 train.head(5)
 
 
-print(train.info(), end='\n\n')
-print(test.info())
+# print(train.info(), end='\n\n')
+# print(test.info())
 
-feature = train['label']
+# feature = train['label']
 
 
-train['premise'] = train['premise'].str.replace('[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]', '')
-test['premise'] = test['premise'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]", "")
-train.head(5)
+# train['premise'] = train['premise'].str.replace('[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]', '')
+# test['premise'] = test['premise'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]", "")
+# train.head(5)
 
-train['hypothesis'] = train['hypothesis'].str.replace('[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]', '')
-test['hypothesis'] = test['hypothesis'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]", "")
-train.head(5)
+# train['hypothesis'] = train['hypothesis'].str.replace('[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]', '')
+# test['hypothesis'] = test['hypothesis'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 0-9]", "")
+# train.head(5)
 
-import os
-import random
-from tqdm import tqdm
+# import os
+# import random
+# from tqdm import tqdm
 
-import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from transformers import TrainingArguments, Trainer
-from transformers import AutoModelForSequenceClassification, AutoConfig, AutoTokenizer
+# import torch
+# import torch.nn.functional as F
+# from torch.utils.data import DataLoader
+# from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
+# from transformers import TrainingArguments, Trainer
+# from transformers import AutoModelForSequenceClassification, AutoConfig, AutoTokenizer
 
-def seed_everything(seed:int = 1004):
-    random.seed(seed)
-    np.random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)  # type: ignore
-    torch.backends.cudnn.deterministic = True  # type: ignore
-    torch.backends.cudnn.benchmark = True  # type: ignore
+# def seed_everything(seed:int = 1004):
+#     random.seed(seed)
+#     np.random.seed(seed)
+#     os.environ["PYTHONHASHSEED"] = str(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)  # type: ignore
+#     torch.backends.cudnn.deterministic = True  # type: ignore
+#     torch.backends.cudnn.benchmark = True  # type: ignore
 
-seed_everything(1004)
+# seed_everything(1004)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(device)
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# print(device)
 
-from kobert.utils import get_tokenizer
-from kobert.pytorch_kobert import get_pytorch_kobert_model
+# from kobert.utils import get_tokenizer
+# from kobert.pytorch_kobert import get_pytorch_kobert_model
 
-MODEL_NAME = 'klue/roberta-large'
+# MODEL_NAME = 'klue/roberta-large'
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-config = AutoConfig.from_pretrained(MODEL_NAME)
-config.num_labels = 3
+# config = AutoConfig.from_pretrained(MODEL_NAME)
+# config.num_labels = 3
 
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=config)
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=config)
 
-# print(model)
-# print(config)
+# # print(model)
+# # print(config)
 
-train_dataset, eval_dataset = train_test_split(train, test_size=0.2, shuffle=True, stratify=train['label'])
+# train_dataset, eval_dataset = train_test_split(train, test_size=0.2, shuffle=True, stratify=train['label'])
 
-tokenized_train = tokenizer(
-    list(train_dataset['premise']),
-    list(train_dataset['hypothesis']),
-    return_tensors="pt",
-    max_length=256, # Max_Length = 190
-    padding=True,
-    truncation=True,
-    add_special_tokens=True
-)
+# tokenized_train = tokenizer(
+#     list(train_dataset['premise']),
+#     list(train_dataset['hypothesis']),
+#     return_tensors="pt",
+#     max_length=256, # Max_Length = 190
+#     padding=True,
+#     truncation=True,
+#     add_special_tokens=True
+# )
 
-tokenized_eval = tokenizer(
-    list(eval_dataset['premise']),
-    list(eval_dataset['hypothesis']),
-    return_tensors="pt",
-    max_length=256,
-    padding=True,
-    truncation=True,
-    add_special_tokens=True
-)
+# tokenized_eval = tokenizer(
+#     list(eval_dataset['premise']),
+#     list(eval_dataset['hypothesis']),
+#     return_tensors="pt",
+#     max_length=256,
+#     padding=True,
+#     truncation=True,
+#     add_special_tokens=True
+# )
 
-tokenized_train['input_ids'][0] # input_ids
-tokenized_train = tokenizer.decode(tokenized_train['input_ids'][0]) # 토큰을 문자로 변환
+# tokenized_train['input_ids'][0] # input_ids
+# tokenized_train = tokenizer.decode(tokenized_train['input_ids'][0]) # 토큰을 문자로 변환
 
-class BERTDataset(torch.utils.data.Dataset):
-    def __init__(self, pair_dataset, label):
-        self.pair_dataset = pair_dataset
-        self.label = label
+# class BERTDataset(torch.utils.data.Dataset):
+#     def __init__(self, pair_dataset, label):
+#         self.pair_dataset = pair_dataset
+#         self.label = label
 
-    def __getitem__(self, idx):
-        item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
-        item['label'] = torch.tensor(self.label[idx])
+#     def __getitem__(self, idx):
+#         item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
+#         item['label'] = torch.tensor(self.label[idx])
         
-        return item
+#         return item
 
-    def __len__(self):
-        return len(self.label)
+#     def __len__(self):
+#         return len(self.label)
     
-def label_to_num(label):
-    label_dict = {"entailment": 0, "contradiction": 1, "neutral": 2, "answer": 3}
-    num_label = []
+# def label_to_num(label):
+#     label_dict = {"entailment": 0, "contradiction": 1, "neutral": 2, "answer": 3}
+#     num_label = []
 
-    for v in label:
-        num_label.append(label_dict[v])
+#     for v in label:
+#         num_label.append(label_dict[v])
     
-    return num_label
+#     return num_label
 
 
-train_label = label_to_num(train_dataset['label'].values)
-eval_label = label_to_num(eval_dataset['label'].values)    
+# train_label = label_to_num(train_dataset['label'].values)
+# eval_label = label_to_num(eval_dataset['label'].values)    
     
-train_dataset = BERTDataset(tokenized_train, train_label)
-eval_dataset = BERTDataset(tokenized_eval, eval_label)
+# train_dataset = BERTDataset(tokenized_train, train_label)
+# eval_dataset = BERTDataset(tokenized_eval, eval_label)
 
-# train_dataset.__len__() # 각 데이터셋의 길이
-# train_dataset.__getitem__(19997) # 특정 인덱스의 데이터를 가져오는 것
-# tokenizer.decode(train_dataset.__getitem__(19997)['input_ids']) # 특정 인덱스의 데이터를 가져오는 것
+# # train_dataset.__len__() # 각 데이터셋의 길이
+# # train_dataset.__getitem__(19997) # 특정 인덱스의 데이터를 가져오는 것
+# # tokenizer.decode(train_dataset.__getitem__(19997)['input_ids']) # 특정 인덱스의 데이터를 가져오는 것
 
-# def compute_metrics(pred):
-#     labels = pred.label_ids
-#     preds = pred.predictions.argmax(-1)
-#     probs = pred.predictions
-#     acc = accuracy_score(labels, preds) 
-#     return {'accuracy': acc}
+# # def compute_metrics(pred):
+# #     labels = pred.label_ids
+# #     preds = pred.predictions.argmax(-1)
+# #     probs = pred.predictions
+# #     acc = accuracy_score(labels, preds) 
+# #     return {'accuracy': acc}
   
-# training_ars = TrainingArguments(
-#     output_dir='./result',
-#     num_train_epochs=7,
-#     per_device_train_batch_size=32,
-#     save_total_limit=5,
-#     save_steps=500,
-#     evaluation_strategy='steps',
-#     eval_steps = 500,
-#     load_best_model_at_end = True,
+# # training_ars = TrainingArguments(
+# #     output_dir='./result',
+# #     num_train_epochs=7,
+# #     per_device_train_batch_size=32,
+# #     save_total_limit=5,
+# #     save_steps=500,
+# #     evaluation_strategy='steps',
+# #     eval_steps = 500,
+# #     load_best_model_at_end = True,
+# # )
+
+# # trainer = Trainer(
+# #     model=model,
+# #     args=training_ars,
+# #     train_dataset=train_dataset,
+# #     eval_dataset=eval_dataset,
+# #     tokenizer=tokenizer,
+# #     compute_metrics=compute_metrics,
+# # )
+# # trainer.train()
+# # model.save_pretrained('./result/best_model')
+
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Tokenizer_NAME = "klue/roberta-large"
+# tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+
+# MODEL_NAME = 'D:\\Study\\results\\checkpoint-500'
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+# model.resize_token_embeddings(tokenizer.vocab_size)
+# model.to(device)
+
+# # print(tokenizer)
+
+
+# test_label = label_to_num(test['label'].values)
+
+# tokenized_test = tokenizer(
+#     list(test['premise']),
+#     list(test['hypothesis']),
+#     return_tensors="pt",
+#     max_length=258,
+#     padding=True,
+#     truncation=True,
+#     add_special_tokens=True
 # )
 
-# trainer = Trainer(
-#     model=model,
-#     args=training_ars,
-#     train_dataset=train_dataset,
-#     eval_dataset=eval_dataset,
-#     tokenizer=tokenizer,
-#     compute_metrics=compute_metrics,
-# )
-# trainer.train()
-# model.save_pretrained('./result/best_model')
+# test_dataset = BERTDataset(tokenized_test, test_label)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-Tokenizer_NAME = "klue/roberta-large"
-tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+# model.eval()
+# output_pred = []
+# output_prob = []
 
-MODEL_NAME = 'D:\\Study\\results\\checkpoint-500'
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
-model.resize_token_embeddings(tokenizer.vocab_size)
-model.to(device)
+# for i, data in enumerate(tqdm(dataloader)):
+#     with torch.no_grad():
+#         outputs = model(
+#             input_ids=data['input_ids'].to(device),
+#             attention_mask=data['attention_mask'].to(device),
+#             token_type_ids=data['token_type_ids'].to(device)
+#         )
+#     logits = outputs[0]
+#     prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
+#     logits = logits.detach().cpu().numpy()
+#     result = np.argmax(logits, axis=-1)
 
-# print(tokenizer)
-
-
-test_label = label_to_num(test['label'].values)
-
-tokenized_test = tokenizer(
-    list(test['premise']),
-    list(test['hypothesis']),
-    return_tensors="pt",
-    max_length=258,
-    padding=True,
-    truncation=True,
-    add_special_tokens=True
-)
-
-test_dataset = BERTDataset(tokenized_test, test_label)
-
-dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-
-model.eval()
-output_pred = []
-output_prob = []
-
-for i, data in enumerate(tqdm(dataloader)):
-    with torch.no_grad():
-        outputs = model(
-            input_ids=data['input_ids'].to(device),
-            attention_mask=data['attention_mask'].to(device),
-            token_type_ids=data['token_type_ids'].to(device)
-        )
-    logits = outputs[0]
-    prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
-    logits = logits.detach().cpu().numpy()
-    result = np.argmax(logits, axis=-1)
-
-    output_pred.append(result)
-    output_prob.append(prob)
+#     output_pred.append(result)
+#     output_prob.append(prob)
   
-pred_answer, output_prob = np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
-print(pred_answer)
+# pred_answer, output_prob = np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
+# print(pred_answer)
 
 
 
-def num_to_label(label):
-    label_dict = {0: "entailment", 1: "contradiction", 2: "neutral"}
-    str_label = []
+# def num_to_label(label):
+#     label_dict = {0: "entailment", 1: "contradiction", 2: "neutral"}
+#     str_label = []
 
-    for i, v in enumerate(label):
-        str_label.append([i,label_dict[v]])
+#     for i, v in enumerate(label):
+#         str_label.append([i,label_dict[v]])
     
-    return str_label
+#     return str_label
 
-answer = num_to_label(pred_answer)
-print(answer)
+# answer = num_to_label(pred_answer)
+# print(answer)
 
-df = pd.DataFrame(answer, columns=['index', 'label'])
+# df = pd.DataFrame(answer, columns=['index', 'label'])
 
-df.to_csv(PATH + '0216_1111.csv', index=False)
+# df.to_csv(PATH + '0216_1111.csv', index=False)
 
-print(df)
+# print(df)
 
 
