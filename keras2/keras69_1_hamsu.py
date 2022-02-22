87a87ad3e01f577372c6c3fca78b8f9f1764b7db
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-02-22 09:50:29 +0900

    Update keras69_1_hamsu.py

--- keras2/keras69_1_hamsu.py
+++ keras2/keras69_1_hamsu.py
@@ -1,1 +1,7 @@
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten,Conv2D, GlobalAveragePooling2D
+from tensorflow.keras.application import VGG16, VGG19, Xception
+
+
+input = Input(shape=(100,100,3))
+VGG16_model = VGG16(include_top=False, input_shape=(100,100,3))
