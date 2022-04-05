3e1cae7f577d0e9730b964ef5851c7ae7697b9c3
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-04-05 17:11:08 +0900

    0405

---
+++ keras_Dacon/news/transeformer.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+import datetime
+#from kaggle.competitions import nflrush
+from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler
+import os
+import re
+import keras
+from sklearn.model_selection import KFold, RepeatedKFold
+from sklearn.isotonic import IsotonicRegression
+tqdm.pandas()
+
+from numba import jit
+
+from kaggle.competitions import nflrush
+
+
+
+
+
+
+
+
+
+
+
