3af58318afeab57b80e322d7f236399502d38f38
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-04-06 10:29:10 +0900

    0406

--- keras_Dacon/news/BERT_IMDB_.py
+++ keras_Dacon/news/BERT_IMDB_.py
@@ -1,30 +1,321 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import matplotlib as mpl
-import datetime
-#from kaggle.competitions import nflrush
-from tqdm import tqdm
-from sklearn.preprocessing import StandardScaler
-import os
 import re
-import keras
-from sklearn.model_selection import KFold, RepeatedKFold
-from sklearn.isotonic import IsotonicRegression
-tqdm.pandas()
+import random
+import time
+import datetime
 
-from numba import jit
+import pandas as pd
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
-from kaggle.competitions import nflrush
+from scipy import stats
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from transformers import get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.datasets import imdb
+from sklearn.model_selection import train_test_split
+
+def separator(text):
+    return re.sub(pattern="<b.*/>|\.|\?|\!", string=text, repl=" [SEP] ")
+
+df = pd.read_csv('imdbdata.csv', header=0)
+df.head(5)
+texts = df.review
+classes = df.sentiment
+classes_oh = df.sentiment.apply(lambda x: 1 if x == 'positive' else 0)
+classes_oh = classes_oh.values
+
+def bert_tokenize(texts):
+    return [tokenizer.tokenize("[CLS] " + text + " [SEP]") for text in texts]
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+tokenized = bert_tokenize(texts)
+bert_ids = [tokenizer.convert_tokens_to_ids(tokens) for tokens in tokenized]
+
+number_of_tokens = np.array([len(bert_id) for bert_id in bert_ids])
+stats.describe(number_of_tokens)
+
+MAX_LEN = 512
+padded_bert_ids = pad_sequences(bert_ids, maxlen=MAX_LEN, dtype='long',
+									truncating='post', padding='post')
+padded_bert_ids[0]
+
+attention_masks = [] # 
+for seq in padded_bert_ids: # 
+    seq_mask = [float(i>0) for i in seq]
+    attention_masks.append(seq_mask)
+
+X_train, X_test, y_train, y_test = \
+train_test_split(padded_bert_ids, classes_oh, random_state=42, test_size=0.3)
+
+masks_train, masks_test, _, _ = train_test_split(attention_masks, padded_bert_ids, 
+                                                 random_state=42, test_size=0.3)
+
+X_train, X_val, y_train, y_val = \
+train_test_split(X_train, y_train, random_state=42, test_size=0.1)
+
+masks_train, masks_val, _, _ = train_test_split(masks_train, masks_train, 
+                                                 random_state=42, test_size=0.1)
 
 
 
 
+# display(
+#     f"X_train: {X_train.shape}",
+#     f"X_val: {X_val.shape}",
+#     f"X_test: {X_test.shape}",
+#     f"y_train: {y_train.shape}",
+#     f"y_val: {y_val.shape}",
+#     f"y_test: {y_test.shape}",
+#     f"masks_train: {len(masks_train)}",
+#     f"masks_val: {len(masks_val)}",
+#     f"masks_test: {len(masks_test)}",
+# )
+
+train_inputs = torch.tensor(X_train)
+train_labels = torch.tensor(y_train)
+train_masks = torch.tensor(masks_train)
+validation_inputs = torch.tensor(X_val)
+validation_labels = torch.tensor(y_val)
+validation_masks = torch.tensor(masks_val)
+
+test_inputs = torch.tensor(X_test)
+test_labels = torch.tensor(y_test)
+test_masks = torch.tensor(masks_test)
+
+print(train_inputs.shape)
+print(train_labels.shape)
+print(train_masks.shape)
+print(validation_inputs.shape)
+print(validation_labels.shape)
+print(validation_masks.shape)
+print(test_inputs.shape)
+print(test_labels.shape)
+print(test_masks.shape)
+
+BATCH_SIZE = 4
+
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=BATCH_SIZE)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=BATCH_SIZE)
+
+test_data = TensorDataset(test_inputs, test_masks, test_labels)
+test_sampler = RandomSampler(test_data)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=BATCH_SIZE)
+
+device = torch.device("cuda:0")
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5, # 학습률
+                  eps = 1e-8 # 0으로 나누는 것을 방지하기 위한 epsilon 값
+                )
+
+epochs = 4
+total_steps = len(train_dataloader) * epochs
+
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = 0,
+                                            num_training_steps = total_steps)
+
+# 정확도 계산 함수
+def flat_accuracy(preds, labels):
+    
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+# 시간 표시 함수
+def format_time(elapsed):
+
+    # 반올림
+    elapsed_rounded = int(round((elapsed)))
+    
+    # hh:mm:ss으로 형태 변경
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+    
+
+# 재현을 위해 랜덤시드 고정
+seed_val = 42
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+# 그래디언트 초기화
+model.zero_grad()
+
+# 에폭만큼 반복
+for epoch_i in range(0, epochs):
+    
+    # ========================================
+    #               Training
+    # ========================================
+    
+    print("")
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training...')
+
+    # 시작 시간 설정
+    t0 = time.time()
+
+    # 로스 초기화
+    total_loss = 0
+
+    # 훈련모드로 변경
+    model.train()
+        
+    # 데이터로더에서 배치만큼 반복하여 가져옴
+    for step, batch in enumerate(train_dataloader):
+        # 경과 정보 표시
+        if step % 500 == 0 and not step == 0:
+            elapsed = format_time(time.time() - t0)
+            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+
+        # 배치를 GPU에 넣음
+        batch = tuple(t.to(device) for t in batch)
+        
+        # 배치에서 데이터 추출
+        b_input_ids, b_input_mask, b_labels = batch
+
+        # Forward 수행                
+        outputs = model(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask, 
+                        labels=b_labels)
+        
+        # 로스 구함
+        loss = outputs[0]
+
+        # 총 로스 계산
+        total_loss += loss.item()
+
+        # Backward 수행으로 그래디언트 계산
+        loss.backward()
+
+        # 그래디언트 클리핑
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # 그래디언트를 통해 가중치 파라미터 업데이트
+        optimizer.step()
+
+        # 스케줄러로 학습률 감소
+        scheduler.step()
+
+        # 그래디언트 초기화
+        model.zero_grad()
+
+    # 평균 로스 계산
+    avg_train_loss = total_loss / len(train_dataloader)            
+
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epcoh took: {:}".format(format_time(time.time() - t0)))
+        
+    # ========================================
+    #               Validation
+    # ========================================
+
+    print("")
+    print("Running Validation...")
+
+    #시작 시간 설정
+    t0 = time.time()
+
+    # 평가모드로 변경
+    model.eval()
+
+    # 변수 초기화
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+
+    # 데이터로더에서 배치만큼 반복하여 가져옴
+    for batch in validation_dataloader:
+        # 배치를 GPU에 넣음
+        batch = tuple(t.to(device) for t in batch)
+        
+        # 배치에서 데이터 추출
+        b_input_ids, b_input_mask, b_labels = batch
+        
+        # 그래디언트 계산 안함
+        with torch.no_grad():     
+            # Forward 수행
+            outputs = model(b_input_ids, 
+                            token_type_ids=None, 
+                            attention_mask=b_input_mask)
+        
+        # 로스 구함
+        logits = outputs[0]
+
+        # CPU로 데이터 이동
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        
+        # 출력 로짓과 라벨을 비교하여 정확도 계산
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+
+    print("  Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
+    print("  Validation took: {:}".format(format_time(time.time() - t0)))
+
+print("")
+print("Training complete!")
 
 
 
+# test
 
+#시작 시간 설정
+t0 = time.time()
 
+# 평가모드로 변경
+model.eval()
 
+# 변수 초기화
+eval_loss, eval_accuracy = 0, 0
+nb_eval_steps, nb_eval_examples = 0, 0
 
+# 데이터로더에서 배치만큼 반복하여 가져옴
+for step, batch in enumerate(test_dataloader):
+    # 경과 정보 표시
+    if step % 100 == 0 and not step == 0:
+        elapsed = format_time(time.time() - t0)
+        print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(test_dataloader), elapsed))
+
+    # 배치를 GPU에 넣음
+    batch = tuple(t.to(device) for t in batch)
+    
+    # 배치에서 데이터 추출
+    b_input_ids, b_input_mask, b_labels = batch
+    
+    # 그래디언트 계산 안함
+    with torch.no_grad():     
+        # Forward 수행
+        outputs = model(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask)
+    
+    # 로스 구함
+    logits = outputs[0]
+
+    # CPU로 데이터 이동
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+    
+    # 출력 로짓과 라벨을 비교하여 정확도 계산
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+    eval_accuracy += tmp_eval_accuracy
+    nb_eval_steps += 1
+
+print("")
+print("Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
+print("Test took: {:}".format(format_time(time.time() - t0)))
