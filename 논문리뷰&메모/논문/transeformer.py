5e2bc4e169eda55d17fb338baa3180b9a1c2653b
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-04-06 16:21:12 +0900

    0406

---
+++ 논문리뷰&메모/논문/transeformer.py
@@ -0,0 +1,362 @@
+import spacy
+
+spacy_en = spacy.load('en') # 영어 토큰화(tokenization)
+spacy_de = spacy.load('de') # 독일어 토큰화(tokenization)
+
+# 간단히 토큰화(tokenization) 기능 써보기
+tokenized = spacy_en.tokenizer("I am a graduate student.")
+
+for i, token in enumerate(tokenized):
+    print(f"인덱스 {i}: {token.text}")
+
+# 독일어(Deutsch) 문장을 토큰화 하는 함수 (순서를 뒤집지 않음)
+def tokenize_de(text):
+    return [token.text for token in spacy_de.tokenizer(text)]
+
+# 영어(English) 문장을 토큰화 하는 함수
+def tokenize_en(text):
+    return [token.text for token in spacy_en.tokenizer(text)]
+
+from torchtext.data import Field, BucketIterator
+
+SRC = Field(tokenize=tokenize_de, init_token="<sos>", eos_token="<eos>", lower=True, batch_first=True)
+TRG = Field(tokenize=tokenize_en, init_token="<sos>", eos_token="<eos>", lower=True, batch_first=True)
+
+from torchtext.datasets import Multi30k
+
+train_dataset, valid_dataset, test_dataset = Multi30k.splits(exts=(".de", ".en"), fields=(SRC, TRG))
+
+SRC.build_vocab(train_dataset, min_freq=2)
+TRG.build_vocab(train_dataset, min_freq=2)
+
+print(f"len(SRC): {len(SRC.vocab)}")
+print(f"len(TRG): {len(TRG.vocab)}")
+
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+BATCH_SIZE = 128
+
+# 일반적인 데이터 로더(data loader)의 iterator와 유사하게 사용 가능
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_dataset, valid_dataset, test_dataset),
+    batch_size=BATCH_SIZE,
+    device=device)
+
+for i, batch in enumerate(train_iterator):
+    src = batch.src
+    trg = batch.trg
+
+    print(f"첫 번째 배치 크기: {src.shape}")
+
+    # 현재 배치에 있는 하나의 문장에 포함된 정보 출력
+    for i in range(src.shape[1]):
+        print(f"인덱스 {i}: {src[0][i].item()}") # 여기에서는 [Seq_num, Seq_len]
+
+    # 첫 번째 배치만 확인
+    break
+
+
+
+import torch.nn as nn
+
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hidden_dim, n_heads, dropout_ratio, device):
+        super().__init__()
+
+        assert hidden_dim % n_heads == 0
+
+        self.hidden_dim = hidden_dim # 임베딩 차원
+        self.n_heads = n_heads # 헤드(head)의 개수: 서로 다른 어텐션(attention) 컨셉의 수
+        self.head_dim = hidden_dim // n_heads # 각 헤드(head)에서의 임베딩 차원
+
+        self.fc_q = nn.Linear(hidden_dim, hidden_dim) # Query 값에 적용될 FC 레이어
+        self.fc_k = nn.Linear(hidden_dim, hidden_dim) # Key 값에 적용될 FC 레이어
+        self.fc_v = nn.Linear(hidden_dim, hidden_dim) # Value 값에 적용될 FC 레이어
+
+        self.fc_o = nn.Linear(hidden_dim, hidden_dim)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+        self.scale = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+
+    def forward(self, query, key, value, mask = None):
+
+        batch_size = query.shape[0]
+
+        # query: [batch_size, query_len, hidden_dim]
+        # key: [batch_size, key_len, hidden_dim]
+        # value: [batch_size, value_len, hidden_dim]
+ 
+        Q = self.fc_q(query)
+        K = self.fc_k(key)
+        V = self.fc_v(value)
+
+        # Q: [batch_size, query_len, hidden_dim]
+        # K: [batch_size, key_len, hidden_dim]
+        # V: [batch_size, value_len, hidden_dim]
+
+        # hidden_dim → n_heads X head_dim 형태로 변형
+        # n_heads(h)개의 서로 다른 어텐션(attention) 컨셉을 학습하도록 유도
+        Q = Q.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+
+        # Q: [batch_size, n_heads, query_len, head_dim]
+        # K: [batch_size, n_heads, key_len, head_dim]
+        # V: [batch_size, n_heads, value_len, head_dim]
+
+        # Attention Energy 계산
+        energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+
+        # energy: [batch_size, n_heads, query_len, key_len]
+
+        # 마스크(mask)를 사용하는 경우
+        if mask is not None:
+            # 마스크(mask) 값이 0인 부분을 -1e10으로 채우기
+            energy = energy.masked_fill(mask==0, -1e10)
+
+        # 어텐션(attention) 스코어 계산: 각 단어에 대한 확률 값
+        attention = torch.softmax(energy, dim=-1)
+
+        # attention: [batch_size, n_heads, query_len, key_len]
+
+        # 여기에서 Scaled Dot-Product Attention을 계산
+        x = torch.matmul(self.dropout(attention), V)
+
+        # x: [batch_size, n_heads, query_len, head_dim]
+
+        x = x.permute(0, 2, 1, 3).contiguous()
+
+        # x: [batch_size, query_len, n_heads, head_dim]
+
+        x = x.view(batch_size, -1, self.hidden_dim)
+
+        # x: [batch_size, query_len, hidden_dim]
+
+        x = self.fc_o(x)
+
+        # x: [batch_size, query_len, hidden_dim]
+
+        return x, attention
+
+
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hidden_dim, pf_dim, dropout_ratio):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(hidden_dim, pf_dim)
+        self.fc_2 = nn.Linear(pf_dim, hidden_dim)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, x):
+
+        # x: [batch_size, seq_len, hidden_dim]
+
+        x = self.dropout(torch.relu(self.fc_1(x)))
+
+        # x: [batch_size, seq_len, pf_dim]
+
+        x = self.fc_2(x)
+
+        # x: [batch_size, seq_len, hidden_dim]
+
+        return x
+
+class EncoderLayer(nn.Module):
+    def __init__(self, hidden_dim, n_heads, pf_dim, dropout_ratio, device):
+        super().__init__()
+
+        self.self_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.ff_layer_norm = nn.LayerNorm(hidden_dim)
+        self.self_attention = MultiHeadAttentionLayer(hidden_dim, n_heads, dropout_ratio, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hidden_dim, pf_dim, dropout_ratio)
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    # 하나의 임베딩이 복제되어 Query, Key, Value로 입력되는 방식
+    def forward(self, src, src_mask):
+
+        # src: [batch_size, src_len, hidden_dim]
+        # src_mask: [batch_size, src_len]
+
+        # self attention
+        # 필요한 경우 마스크(mask) 행렬을 이용하여 어텐션(attention)할 단어를 조절 가능
+        _src, _ = self.self_attention(src, src, src, src_mask)
+
+        # dropout, residual connection and layer norm
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        # position-wise feedforward
+        _src = self.positionwise_feedforward(src) 
+
+        # dropout, residual and layer norm
+        src = self.ff_layer_norm(src + self.dropout(_src))
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        return src
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout_ratio, device, max_length=100):
+        super().__init__()
+
+        self.device = device
+
+        self.tok_embedding = nn.Embedding(input_dim, hidden_dim)
+        self.pos_embedding = nn.Embedding(max_length, hidden_dim)
+
+        self.layers = nn.ModuleList([EncoderLayer(hidden_dim, n_heads, pf_dim, dropout_ratio, device) for _ in range(n_layers)])
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+        self.scale = torch.sqrt(torch.FloatTensor([hidden_dim])).to(device)
+
+    def forward(self, src, src_mask):
+
+        # src: [batch_size, src_len]
+        # src_mask: [batch_size, src_len]
+
+        batch_size = src.shape[0]
+        src_len = src.shape[1]
+
+        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+
+        # pos: [batch_size, src_len]
+
+        # 소스 문장의 임베딩과 위치 임베딩을 더한 것을 사용
+        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        # 모든 인코더 레이어를 차례대로 거치면서 순전파(forward) 수행
+        for layer in self.layers:
+            src = layer(src, src_mask)
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        return src # 마지막 레이어의 출력을 반환
+
+class DecoderLayer(nn.Module):
+    def __init__(self, hidden_dim, n_heads, pf_dim, dropout_ratio, device):
+        super().__init__()
+
+        self.self_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.enc_attn_layer_norm = nn.LayerNorm(hidden_dim)
+        self.ff_layer_norm = nn.LayerNorm(hidden_dim)
+        self.self_attention = MultiHeadAttentionLayer(hidden_dim, n_heads, dropout_ratio, device)
+        self.encoder_attention = MultiHeadAttentionLayer(hidden_dim, n_heads, dropout_ratio, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hidden_dim, pf_dim, dropout_ratio)
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    # 인코더의 출력 값(enc_src)을 어텐션(attention)하는 구조
+    def forward(self, trg, enc_src, trg_mask, src_mask): # forward는 인코더의 출력과 디코더의 입력을 받아서 순전파를 수행하는 구조
+
+        # trg: [batch_size, trg_len, hidden_dim]
+        # enc_src: [batch_size, src_len, hidden_dim]
+        # trg_mask: [batch_size, trg_len]
+        # src_mask: [batch_size, src_len]
+
+        # self attention
+        # 자기 자신에 대하여 어텐션(attention)
+        _trg, _ = self.self_attention(trg, trg, trg, trg_mask)
+
+        # dropout, residual connection and layer norm
+        trg = self.self_attn_layer_norm(trg + self.dropout(_trg))
+
+        # trg: [batch_size, trg_len, hidden_dim]
+
+        # encoder attention
+        # 디코더의 쿼리(Query)를 이용해 인코더를 어텐션(attention)
+        _trg, attention = self.encoder_attention(trg, enc_src, enc_src, src_mask)
+
+        # dropout, residual connection and layer norm
+        trg = self.enc_attn_layer_norm(trg + self.dropout(_trg))
+
+        # trg: [batch_size, trg_len, hidden_dim]
+
+        # positionwise feedforward
+        _trg = self.positionwise_feedforward(trg)
+
+        # dropout, residual and layer norm
+        trg = self.ff_layer_norm(trg + self.dropout(_trg))
+
+        # trg: [batch_size, trg_len, hidden_dim]
+        # attention: [batch_size, n_heads, trg_len, src_len]
+
+        return trg, attention
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout_ratio, device, max_length=100):
+        super().__init__()
+
+        self.device = device
+
+        self.tok_embedding = nn.Embedding(output_dim, hidden_dim)
+        self.pos_embedding = nn.Embedding(max_length, hidden_dim)
+
+        self.layers = nn.ModuleList([DecoderLayer(hidden_dim, n_heads, pf_dim, dropout_ratio, device) for _ in range(n_layers)])
+
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+        self.scale = torch.sqrt(torch.FloatTensor([hidden_dim])).to(device)
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+
+        # trg: [batch_size, trg_len]
+        # enc_src: [batch_size, src_len, hidden_dim]
+        # trg_mask: [batch_size, trg_len]
+        # src_mask: [batch_size, src_len]
+
+        batch_size = trg.shape[0]
+        trg_len = trg.shape[1]
+
+        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+
+        # pos: [batch_size, trg_len]
+
+        trg = self.dropout((self.tok_embedding(trg) * self.scale) + self.pos_embedding(pos))
+
+        # trg: [batch_size, trg_len, hidden_dim]
+
+        for layer in self.layers:
+            # 소스 마스크와 타겟 마스크 모두 사용
+            trg, attention = layer(trg, enc_src, trg_mask, src_mask)
+
+        # trg: [batch_size, trg_len, hidden_dim]
+        # attention: [batch_size, n_heads, trg_len, src_len]
+
+        output = self.fc_out(trg)
+
+        # output: [batch_size, trg_len, output_dim]
+
+        return output, attention
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
