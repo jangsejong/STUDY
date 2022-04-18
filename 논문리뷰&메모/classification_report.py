b14240c96519dca3c0d9384ce5bd8969a77adc6b
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-04-19 02:26:02 +0900

    0419

---
+++ 논문리뷰&메모/classification_report.py
@@ -0,0 +1,32 @@
+from sklearn.metrics import classification_report
+y_true = [0, 1, 2, 2, 2]
+y_pred = [0, 0, 2, 2, 1]
+target_names = ['class 0', 'class 1', 'class 2']
+print(classification_report(y_true, y_pred, target_names=target_names)) 
+# 
+'''
+              precision    recall  f1-score   support
+
+     class 0       0.50      1.00      0.67         1
+     class 1       0.00      0.00      0.00         1
+     class 2       1.00      0.67      0.80         3
+
+    accuracy                           0.60         5
+   macro avg       0.50      0.56      0.49         5
+weighted avg       0.70      0.60      0.61         5
+'''
+
+y_pred = [1, 1, 0]
+y_true = [1, 1, 1]
+print(classification_report(y_true, y_pred, labels=[1, 2, 3]))
+'''
+              precision    recall  f1-score   support
+
+           1       1.00      0.67      0.80         3
+           2       0.00      0.00      0.00         0
+           3       0.00      0.00      0.00         0
+
+   micro avg       1.00      0.67      0.80         3
+   macro avg       0.33      0.22      0.27         3
+weighted avg       1.00      0.67      0.80         3
+'''
--- 논문리뷰&메모/detect.py
+++ 논문리뷰&메모/detect.py
@@ -135,7 +135,7 @@ def run(
                 p, im0, frame = path[i], im0s[i].copy(), dataset.count
                 s += f'{i}: '
             else:
-                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0) # frame : frame index
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # im.jpg
