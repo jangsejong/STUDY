5e334f5f29a938c6631ccb4df8e5e6fe3ad5f55d
jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-05-16 10:33:36 +0900

    0516

--- torch/torch08_class_cancer.py
+++ torch/torch08_class_cancer.py
@@ -1,6 +1,6 @@
 from pickletools import optimize
 import datasets
-from sklearn.datasets import load_breast_cancer, load_iris
+from sklearn.datasets import load_breast_cancer
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,31 +10,28 @@ DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
 print('torch :', torch.__version__, ' 사용DEVICE :', DEVICE)
 
 #1. 데이터
-datasets = load_iris()
+datasets = load_breast_cancer()
 
 x = datasets.data
 y = datasets.target
-print(x.shape, y.shape) # (150, 4) (150,)
+
 # x = torch.FloatTensor(x)
-# y = torch.LongTensor(y)
+# y = torch.FloatTensor(y)
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
 
 x_train = torch.FloatTensor(x_train)
-# y_train = torch.FloatTensor(y_train).unsqueeze(1).to(DEVICE)
-y_train = torch.LongTensor(y_train).to(DEVICE)
-
+y_train = torch.FloatTensor(y_train).unsqueeze(1).to(DEVICE)
 x_test = torch.FloatTensor(x_test)
-# y_test = torch.FloatTensor(y_test).unsqueeze(1).to(DEVICE)
-y_test = torch.LongTensor(y_test).to(DEVICE)
+y_test = torch.FloatTensor(y_test).unsqueeze(1).to(DEVICE)
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-print(x_train.shape, y_train.shape) # (105, 4)
+print(x_train.shape, y_train.shape) # (398, 30) torch.Size([398])
 print(type(x_train), type(y_train)) # <class 'numpy.ndarray'> <class 'torch.Tensor'>
 
 x_train = torch.FloatTensor(x_train).to(DEVICE)
@@ -42,30 +39,52 @@ x_test = torch.FloatTensor(x_test).to(DEVICE)
 print(type(x_train), type(y_train))
 
 #2. 모델구성
-model = nn.Sequential(
-    nn.Linear(4, 32),
-    nn.ReLU(),
-    nn.Linear(32, 32),
-    nn.ReLU(),
-    nn.Linear(32, 16),
-    nn.ReLU(),
-    nn.Linear(16, 16),
-    nn.ReLU(),
-    nn.Linear(16, 16),
-    nn.ReLU(),
-    nn.Linear(16, 3),
-    # nn.Sigmoid(),
-).to(DEVICE)
+# model = nn.Sequential(
+#     nn.Linear(30, 32),
+#     nn.ReLU(),
+#     nn.Linear(32, 32),
+#     nn.ReLU(),
+#     nn.Linear(32, 16),
+#     nn.ReLU(),
+#     nn.Linear(16, 16),
+#     nn.ReLU(),
+#     nn.Linear(16, 16),
+#     nn.ReLU(),
+#     nn.Linear(16, 1),
+#     nn.Sigmoid(),
+# ).to(DEVICE)
+
+class Model(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        # super(Model, self).__init__()
+        self.layer1 = nn.Linear(input_size, 32)
+        self.layer2 = nn.Linear(32, 32)
+        self.layer3 = nn.Linear(32, 16)
+        self.layer4 = nn.Linear(16, 16)
+        self.layer5 = nn.Linear(16, 16)
+        self.layer6 = nn.Linear(16, output_size)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, input_dim):
+        out1 = self.layer1(input_dim)
+        out2 = self.layer2(out1)
+        out3 = self.layer3(out2)
+        out4 = self.layer4(out3)
+        out5 = self.layer5(out4)
+        out = self.layer6(out5)
+        out = self.sigmoid(out)
+        return out
+        
+model = Model(30, 1).to(DEVICE)
 
 #3. 컴파일, 훈련
 
-# criterion = nn.BCELoss()
-criterion = nn.CrossEntropyLoss()
+criterion = nn.BCELoss()
 
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 def train(model, criterion, optimizer, x_train, y_train, batch_size=1):
-    # model.train()
     optimizer.zero_grad()
     
     hypothesis = model(x_train)
@@ -94,8 +113,7 @@ def evaluate(model, criterion, x_test, y_test):
 loss2 = evaluate(model, criterion, x_test, y_test)
 print('최종 loss: ', loss2)
 
-# y_predict = (model(x_test) > 0.5).float()
-y_predict = torch.argmax(model(x_test), dim=1)
+y_predict = (model(x_test) > 0.5).float()
 print(y_predict)
 
 scores = (y_predict == y_test).float().mean()
@@ -109,11 +127,6 @@ scores3 = accuracy_score(y_test.cpu().detach().numpy(), y_predict.cpu().detach()
 print('정확도: ', scores2)
 
 '''
-최종 loss:  0.923826277256012
-tensor([1, 2, 1, 0, 1, 1, 0, 0, 0, 2, 2, 2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 1, 2,
-        1, 2, 0, 1, 2, 2, 0, 0, 2, 0, 1, 0, 1, 0, 2, 1, 0, 2, 2, 1, 1],
-       device='cuda:0')
-정확도:  tensor(0.9111, device='cuda:0')
-정확도:  0.9111111111111111
-정확도:  0.9111111111111111
+정확도:  0.9590643274853801
+정확도:  0.9590643274853801
 '''
