ac038aa2197bbc801f18e201eff9c78c91428f09
Dr.Jang <92250663+jangsejong@users.noreply.github.com>
2022-07-13 08:29:19 +0700

    Create 02_cube_collection.py

---
+++ Pymel/camera/02_cube_collection.py
@@ -0,0 +1,9 @@
+import maya.cmds as cmds
+import random
+         
+for n in range(25):
+    cube, cubeShape = cmds.polyCube()
+    x = random.randrange(-50, 50)
+    y = random.randrange(-50, 50)
+    z = random.randrange(-50, 50)
+    cmds.xform(cube, t = (x,y,z))
