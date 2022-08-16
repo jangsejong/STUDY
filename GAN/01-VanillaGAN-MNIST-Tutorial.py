63f1b9db957773bbbcf8d09c98bf01c2adb34f75
Jangsejong <92250663+jangsejong@users.noreply.github.com>
2022-08-16 08:04:57 +0700

    Update and rename 01-VanillaGAN-MNIST-Tutorial.ipynb to 01-VanillaGAN-MNIST-Tutorial.py

--- GAN/01-VanillaGAN-MNIST-Tutorial.py
+++ GAN/01-VanillaGAN-MNIST-Tutorial.py
@@ -1,5 +1,5 @@
-%%javascript
-IPython.OutputArea.auto_scroll_threshold = 9999;
+# %%javascript
+# IPython.OutputArea.auto_scroll_threshold = 9999;
 
 from tensorflow.keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
